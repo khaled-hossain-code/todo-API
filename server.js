35c8e51759c0a98b8d69748260f8da29eb093058
Khaled Hossain <md.khaled.eee@gmail.com>
2016-04-09 10:14:06 +0600

    search query API completed

--- server.js
+++ server.js
@@ -17,9 +17,21 @@ app.get('/', function (req, res) {
   res.send('Todo API Root');
 });
 
-//GET /todos
+//GET /todos?completed=true
+//GET /todos?completed=false
 app.get('/todos', function (req, res) {
-  res.json(todos);
+  var queryParams = req.query;
+  var filteredTodos = todos;
+
+  if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true')
+  {
+    filteredTodos = _.where(filteredTodos, {completed:true});
+  }else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false')
+  {
+    filteredTodos = _.where(filteredTodos, {completed:false});
+    console.log('inside false');
+  }
+  res.json(filteredTodos);
 });
 
 //GET /todos/:id
